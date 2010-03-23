8e36f5768cb9a989117b6a351275c29db5e00d1b
Ron van Weverwijk <rolin@MacBook-Pro-van-Ron-van-Weverwijk.local>
2010-03-23 22:49:25 +0100

    windows dir toegevoegd

--- src/main/java/nl/weverwijk/photo/rest/PhotoRestService.java
+++ src/main/java/nl/weverwijk/photo/rest/PhotoRestService.java
@@ -14,8 +14,8 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/photo")
 public class PhotoRestService {
-
-	private static final String PHOTO_BASE_LOCATION = "/Users/rolin/projects/rweverwijk/restPhotoService/src/main/webapp/images";
+	//private static final String PHOTO_BASE_LOCATION = "D:\\data\\documenten\\My Pictures\\2009_01_09\\jpg";
+	private static final String PHOTO_BASE_LOCATION = "/Users/rolin/projects/rweverwijk/restPhotoService/src/main/webapp/images";	
 
 	@GET
 	@Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -26,18 +26,16 @@ public class PhotoRestService {
 		if (files != null) {
 			for (String fileName : files) {
 				if (fileName.endsWith(".jpg")) {
-					Photo photo = new Photo(fileName, fileName, "pietje"
-							+ fileName, "images/" + fileName, "images/small/"
-							+ fileName);
+					Photo photo = new Photo(fileName, fileName, "pietje" + fileName, "images/" + fileName, "images/small/" + fileName);
 					result.add(photo);
 				}
 			}
 		}
 		return result;
 	}
-
+	
 	@GET
-	@Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
 	@Path("{id}/")
 	public Photo getPhoto(@PathParam("id") String id) {
 		List<Photo> result = new ArrayList<Photo>();
@@ -45,20 +43,19 @@ public class PhotoRestService {
 		String[] files = path.list();
 		if (files != null) {
 			for (String fileName : files) {
-				Photo photo = new Photo(fileName, fileName,
-						"pietje" + fileName, "images/" + fileName,
-						"images/small/" + fileName);
+				Photo photo = new Photo(fileName, fileName, "pietje" + fileName, "images/" + fileName, "images/small/" + fileName);
 				result.add(photo);
 			}
 		}
 		return result.get(0);
 	}
-
+	
+	
 	@POST
-	@Consumes( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public void savePhoto(Photo photo) {
 		if (photo != null) {
-			System.out.println("SavePhoto: " + photo);
+		System.out.println("SavePhoto: " + photo);
 		} else {
 			System.out.println("SavePhoto: photo is null");
 		}
