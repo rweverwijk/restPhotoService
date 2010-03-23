836b084427d7d0a1a6fbdb93ebaeb9ee0e9f35ae
Ron van Weverwijk <rolin@MacBook-Pro-van-Ron-van-Weverwijk.local>
2010-03-23 22:26:01 +0100

    directory toegevoegd

--- src/main/java/nl/weverwijk/photo/rest/PhotoRestService.java
+++ src/main/java/nl/weverwijk/photo/rest/PhotoRestService.java
@@ -12,18 +12,21 @@ import javax.ws.rs.core.MediaType;
 @Path("/photo")
 public class PhotoRestService {
 
-	private static final String PHOTO_BASE_LOCATION = "D:\\data\\documenten\\My Pictures\\2009_01_09\\jpg";
+	private static final String PHOTO_BASE_LOCATION = "/Users/rolin/projects/rweverwijk/restPhotoService/src/main/webapp/images";
 
 	@GET
-	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	@Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<Photo> getPhotos() {
 		List<Photo> result = new ArrayList<Photo>();
 		File path = new File(PHOTO_BASE_LOCATION);
 		String[] files = path.list();
 		if (files != null) {
 			for (String fileName : files) {
-				Photo photo = new Photo(fileName, "pietje" + fileName, "images/" + fileName, "images/small/" + fileName);
-				result.add(photo);
+				if (fileName.endsWith(".jpg")) {
+					Photo photo = new Photo(fileName, "pietje" + fileName,
+							"images/" + fileName, "images/small/" + fileName);
+					result.add(photo);
+				}
 			}
 		}
 		return result;
